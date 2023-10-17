66ee975a1504d70f53322cd337a6bea4cc84dd0a
agustinmenre@gmail.com <agustinmenre@gmail.com>
2023-10-17 20:28:47 -0300

    Disparos
    -Disparo principal
    -Disparo secundario
    -Efectos de sonido

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -1,4 +1,3 @@
-## Player.gd 
 class_name Player
 extends RigidBody2D
 
@@ -10,7 +9,19 @@ export var potencia_rotacion:int = 900
 var empuje:Vector2 = Vector2.ZERO
 var dir_rotacion:int = 0
 
+## Atributos onready
+onready var canion:Canion = $Canion
+onready var laser:RayoLaser = $LaserBeam2D
+
 ## Metodos
+func _unhandled_input(event: InputEvent) -> void:
+	## Disparo Rayo
+	if event.is_action_pressed("disparo_secundario"):
+		laser.set_is_casting(true)
+		
+		if event.is_action_released("disparo_secundario"):
+			laser.set_is_casting(false)
+
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 	apply_central_impulse(empuje.rotated(rotation))
 	apply_torque_impulse(dir_rotacion * potencia_rotacion)
@@ -33,3 +44,10 @@ func player_input() -> void:
 		dir_rotacion -= 1
 	elif Input.is_action_pressed("rotar_horario"):
 		dir_rotacion += 1
+	
+	# Disparo
+	if Input.is_action_pressed("disparo_principal"):
+		canion.set_esta_disparando(true)
+	if Input.is_action_just_released("disparo_principal"):
+		canion.set_esta_disparando(false)
+
